4ee1ef2e740d6e4268b7b57a35d7e63e245f7768
Ivan <tushchukivan@gmail.com>
2024-10-30 21:57:26 +0200

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -5,7 +5,37 @@
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
 function fetchUsers() {
-  // Ваш код
+  const options = {
+    hostname: 'jsonplaceholder.typicode.com',
+    path: '/users',
+    method: 'GET'
+};
+
+return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            try {
+                const users = JSON.parse(data);
+                const result = users.map(user => ({ id: user.id, name: user.name }));
+                resolve(result);
+            } catch (error) {
+                reject(error);  
+            }
+        });
+    });
+
+    req.on('error', (error) => {
+        reject(error);  
+    });
+
+    req.end();
+});
 }
 
 console.log(fetchUsers())
