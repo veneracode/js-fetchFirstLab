16ecbf9f4223814f32a3828f84cadd675c39dd1c
Ivan <tushchukivan@gmail.com>
2024-10-30 21:57:37 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -8,7 +8,43 @@
 
 
 function createUser(user) {
-  // Ваш код
+  const data = JSON.stringify(user);
+
+    const options = {
+        hostname: 'jsonplaceholder.typicode.com',
+        path: '/users',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            let responseData = '';
+
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const response = JSON.parse(responseData);
+                    resolve(response);
+                } catch (error) {
+                    reject(error);  
+                }
+            });
+        });
+
+        req.on('error', (error) => {
+            reject(error);  
+        });
+
+        req.write(data);
+        req.end();
+    });
 }
 
 console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
